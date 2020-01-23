259adb111e75a503b4b9624aaae35d822b54298d
rizwa <rizwankassamali1@gmail.com>
2020-01-23 14:52:29 -0500

    Edit

--- src/SpyProgramDecrypt.java
+++ src/SpyProgramDecrypt.java
@@ -7,130 +7,122 @@ import java.util.Random;
 import java.lang.Character;
 
 // The "SpyProgramDecrypt" class.
-public class SpyProgramDecrypt
-{
-    public static void main (String[] args)
-    {
-	
-       //self test
+public class SpyProgramDecrypt {
+	public static void main(String[] args) {
 
-	//asks user in input a phrase to be encrypted
-	String test = JOptionPane.showInputDialog ("Enter the phrase to be decrypted.");
+		// self test
 
-	//gets encrypted string
-	String output = decrypt (test);
+		// asks user in input a phrase to be encrypted
+		String test = JOptionPane.showInputDialog("Enter the phrase to be decrypted.");
 
-	//oupts encrypted string 
-	JOptionPane.showMessageDialog (null, output);
+		// gets encrypted string
+		String output = decrypt(test);
 
-    
-    } // main method
-    
-	public static String decrypt (String enc)
-    {
-	//length of the encrypted string
-	int len = enc.length () - 1;
+		// oupts encrypted string
+		JOptionPane.showMessageDialog(null, output);
 
-	//char for the multiplier 
-	char mult = '0';
+	} // main method
 
-	//removes the last char
-	String unenc = enc.substring (0, enc.length () - 1);
+	public static String decrypt(String enc) {
+		// length of the encrypted string
+		int len = enc.length() - 1;
 
-	//string for the random letters 
-	String randLets = "";
+		// char for the multiplier
+		char mult = '0';
 
-	//this loop finds the first number (the multiplier), and remvoes all the random leters
-	for (int l = 0 ; l <= len ; l++)
-	{
-	    //gets char at each instence
-	    char keyChar = unenc.charAt (l);
+		// removes the last char
+		String unenc = enc.substring(0, enc.length() - 1);
 
-	    //if the char is a digit
-	    if (Character.isDigit (keyChar) == true)
-	    {
-		//then we know the multiplier is ther first number
-		mult = unenc.charAt (l);
+		// string for the random letters
+		String randLets = "";
 
-		//breaks the loop
-		break;
-	    }
-	    //if char is not a number then adds the char to the string of random leters 
-	    else
-	    {
-		//adds char to string of random letters 
-		randLets = randLets + keyChar;
-	    }
-	}
+		// this loop finds the first number (the multiplier), and remvoes all the random
+		// leters
+		for (int l = 0; l <= len; l++) {
+			// gets char at each instence
+			char keyChar = unenc.charAt(l);
 
-	//string for the multiplier value
-	String multVal = "";
+			// if the char is a digit
+			if (Character.isDigit(keyChar) == true) {
+				// then we know the multiplier is ther first number
+				mult = unenc.charAt(l);
 
-	//adds the multiplier value to string 
-	multVal = multVal + mult;
+				// breaks the loop
+				break;
+			}
+			// if char is not a number then adds the char to the string of random leters
+			else {
+				// adds char to string of random letters
+				randLets = randLets + keyChar;
+			}
+		}
 
-	//converts the multiplier into and integer
-	int multInt = Integer.parseInt (multVal);
+		// string for the multiplier value
+		String multVal = "";
 
-	//length of the random characters 
-	int lenRL = randLets.length ();
+		// adds the multiplier value to string
+		multVal = multVal + mult;
 
-	//remvoves the random characters to start and the multiplier value
-	String raw = unenc.substring (lenRL + 1);
+		// converts the multiplier into and integer
+		int multInt = Integer.parseInt(multVal);
 
-	//undos the reverse
-	raw = new StringBuffer (raw).reverse ().toString ();
+		// length of the random characters
+		int lenRL = randLets.length();
 
-	//divides by the random multiplier and converts back to deciaml from oct 
-	String stringAsc = new BigDecimal (new BigInteger (raw, 8)).divide (new BigDecimal (multInt), 0).toString ();
+		// remvoves the random characters to start and the multiplier value
+		String raw = unenc.substring(lenRL + 1);
 
-	//length of the ascii string
-	int asLen = stringAsc.length ();
+		// undos the reverse
+		raw = new StringBuffer(raw).reverse().toString();
 
-	//string for final output
-	String done = "";
+		// divides by the random multiplier and converts back to deciaml from oct
+		String stringAsc = new BigDecimal(new BigInteger(raw, 8)).divide(new BigDecimal(multInt), 0).toString();
 
-	//loop to check all the ascii values and convernt the string of numbers back into words 
-	for (int h = 0 ; h < asLen ; h++)
-	{
-	    //variable to receive the letter from the ascii value
-	    char let = ' '; 
+		// length of the ascii string
+		int asLen = stringAsc.length();
 
-	    //declaring int variables, value is the ascii value for the char (+52 due to shift), check is used to find if the ascii is 2 or 3 digits
-	    int value = 0, check = 0;
-	    
-	    //gets the number value at each instence
-	    check = Character.getNumericValue (stringAsc.charAt (h)); 
+		// string for final output
+		String done = "";
 
-	    //if the value is one, then the ascii value is triple digit
-	    if (check == 1) 
-	    {
-		//this adds the following two digits, but not mathematically just after each other
-		value = Integer.parseInt (String.valueOf (stringAsc.charAt (h)) + String.valueOf (stringAsc.charAt (h + 1)) + String.valueOf (stringAsc.charAt (h + 2)));
-		
-		//since 2 extra chars were used we can skip the next 2 chars
-		h += 2; 
-	    }
-	    
-	    //the rest of the ascii vals would be 2 digits 
-	    else  
-	    {
-		//since the ascii value is 2 digits, we add the current and next value, but after each other not matjematically 
-		value = Integer.parseInt (String.valueOf (stringAsc.charAt (h)) + String.valueOf (stringAsc.charAt (h + 1))); 
-		h += 1; 
-	    }
+		// loop to check all the ascii values and convernt the string of numbers back
+		// into words
+		for (int h = 0; h < asLen; h++) {
+			// variable to receive the letter from the ascii value
+			char let = ' ';
 
-	    //gets the letter from the ascii value, i have to minus 52 bevuase 52 was added in the encryption
-	    let = (char) (value - 53);
+			// declaring int variables, value is the ascii value for the char (+52 due to
+			// shift), check is used to find if the ascii is 2 or 3 digits
+			int value = 0, check = 0;
 
-	    //adds all new letters to string
-	    done = done + let;
-	}
+			// gets the number value at each instence
+			check = Character.getNumericValue(stringAsc.charAt(h));
 
-	//return the user's original input
-	return done;
+			// if the value is one, then the ascii value is triple digit
+			if (check == 1) {
+				// this adds the following two digits, but not mathematically just after each
+				// other
+				value = Integer.parseInt(String.valueOf(stringAsc.charAt(h)) + String.valueOf(stringAsc.charAt(h + 1))
+						+ String.valueOf(stringAsc.charAt(h + 2)));
 
-    }//decryption method
-    
-    
+				// since 2 extra chars were used we can skip the next 2 chars
+				h += 2;
+			}
+
+			// the rest of the ascii vals would be 2 digits
+			else {
+				// since the ascii value is 2 digits, we add the current and next value, but
+				// after each other not matjematically
+				value = Integer.parseInt(String.valueOf(stringAsc.charAt(h)) + String.valueOf(stringAsc.charAt(h + 1)));
+				h += 1;
+			}
+
+			let = (char) (value - 53);
+
+			// adds all new letters to string
+			done = done + let;
+		}
+
+		return done;
+
+	}// decryption method
 } // SpyProgramDecrypt class
